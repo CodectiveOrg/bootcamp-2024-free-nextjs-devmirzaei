6b32a24b75fd80902e9962706ddc8b88651bc4bb
Morteza Mirzaei <dev.mirzaei@gmail.com>
2025-01-01 21:42:45 +0330

    [VERCEL] update demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home(): ReactElement {
     <div className={styles.home}>
       <h1>
         <HelloDoctorLogo />
-        سلام دکتر
+        نمایش دمو
       </h1>
       <GlobalSearchBoxComponent />
       <div className={styles.history}>
